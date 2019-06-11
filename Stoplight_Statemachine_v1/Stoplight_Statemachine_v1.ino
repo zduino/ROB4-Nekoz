38132e6e87532294c160f8bc436e68da6c5c359e
zduino <zacke.palmer@gmail.com>
2019-06-11 16:52:50 -0700

    Update Stoplight_Statemachine_v1.ino

--- Stoplight_Statemachine_v1/Stoplight_Statemachine_v1.ino
+++ Stoplight_Statemachine_v1/Stoplight_Statemachine_v1.ino
@@ -32,7 +32,7 @@ int Next_State;
 // The setup routine runs once (when the board turns on, when you press reset, or when
 // you program the board) 
 void setup() {
-    // Set pins with LEDs connected to outputs
+    // Set pins with LEDs connected as outputs
     pinMode(LED_NS_RED, OUTPUT);
     pinMode(LED_NS_YELLOW, OUTPUT);
     pinMode(LED_NS_GREEN, OUTPUT);
