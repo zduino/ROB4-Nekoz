4a8aeff53cca4654aefc654ef9ad92cc1bf17fe4
zduino <zacke.palmer@gmail.com>
2019-06-13 14:48:53 -0700

    Update Time_Game.ino

--- Time_Game/Time_Game.ino
+++ Time_Game/Time_Game.ino
@@ -40,7 +40,7 @@ void loop() {
     if (Debounce_Btn_Time <= millis()) {             // Check if button is bouncing
         if (Btn_State == true) {                     // Check if button state is currently being pressed
             if (Btn_State_Last == false) {           // Check if previous button state was not being pressed
-                onPress();
+                onP // call event function onPress
                 Debounce_Btn_Time = millis() + 20;   // Set time to ignore button bouncing
             }
         }
@@ -50,32 +50,45 @@ void loop() {
 }
 
 void onPress() {
-    if (Playing_Game) {
-
+    if (Playing_Game) { // if you are playing the game
+        =// record time game ends
+        (); // display the score
+        Playing_Game = false; // end the game
     }
     else {
-        digitalWrite(LED_RED, LOW);
+        // turn blue LED on
+        digitalWrite(LED_RED, );
+        digitalWrite(LED_YELLOW, );
+        digitalWrite(LED_GREEN, );
+        digitalWrite(LED_BLUE, );
 
-
-        Playing_Game = true;
-        Game_Start = millis();
+        = true; // begin the game
+        Game_Start = millis(); // record time game starts
     }
 }
 
 void displayScore() {
-    unsigned long diff = abs( (Game_Stop - Game_Start) - GOAL_TIME );
+    long diff = abs( (long)(Game_Stop - Game_Start) - GOAL_TIME );
 
-    if (diff < 1000) {
-        digitalWrite(LED_RED, LOW);
-
+    if (diff < 500) {
+        // light green
+        digitalWrite(LED_RED, );
+        digitalWrite(LED_YELLOW, );
+        digitalWrite(LED_GREEN, );
+        digitalWrite(LED_BLUE, LOW);
     }
-    else if (diff < 2000) {
-
+    else if (diff < 1000) {
+        // light yellow
+        digitalWrite(LED_RED, );
+        digitalWrite(LED_YELLOW, );
+        digitalWrite(LED_GREEN, );
         digitalWrite(LED_BLUE, LOW);
     }
     else {
-        digitalWrite(LED_RED, HIGH);
-        digitalWrite(LED_YELLOW, LOW);
-
+        // light red
+        digitalWrite(LED_RED, );
+        digitalWrite(LED_YELLOW, );
+        digitalWrite(LED_GREEN, );
+        digitalWrite(LED_BLUE, LOW);
     }
 }
